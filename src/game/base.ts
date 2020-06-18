3f1c0bc968bab4874166fa5934da745b54d8afcc
FL3SH <flesh.mm.work@gmail.com>
2020-06-19 01:26:51 +0200

    Hook test*

--- src/game/base.ts
+++ src/game/base.ts
@@ -178,14 +178,16 @@ export class Game implements IGame {
         this.onExpired.push(cb);
     }
 
-
     public nextRoundNumber() {
-       if (this.difficulty === GameDifficulty.Noob) {
-           console.log("base - incrementRoundNumber - noob");
-           return 1;
-       } else {
-           console.log("base - incrementRoundNumber - ", this.state.currentCount);
-           return this.state.currentCount + 1;
-       }
-   } 
+        if (this.difficulty === GameDifficulty.Noob) {
+            console.log("base - incrementRoundNumber - noob");
+            return 1;
+        } else {
+            console.log(
+                "base - incrementRoundNumber - ",
+                this.state.currentCount,
+            );
+            return this.state.currentCount + 1;
+        }
+    }
 }
